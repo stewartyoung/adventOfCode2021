ec1098a5feda3196d0393c897bef2648e5a55db2
Stewart Young <stewartyoung97@gmail.com>
2021-12-02 11:51:01 +0000

    convert array of strings to array of ints

--- 1/answer.ts
+++ 1/answer.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env ts-node
 import * as fs from 'fs';
 const file = fs.readFileSync("input.txt", "utf8");
-const nums = file.split("\n");
+const nums = file.split("\n").map(Number);
 let current = 0;
 let next = 1;
 let largerCount = 0;
 while (next < nums.length) {
     // use unary operator + to convert to number
-    if (+nums[current] < +nums[next]) {
+    if (nums[current] < nums[next]) {
         largerCount += 1;
     }
     current +=1;
     next += 1;
 }
 console.log(largerCount);
+
+// need a sliding window of size 3
+console.log(nums.slice(0, 3));

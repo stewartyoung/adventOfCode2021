7422a47365f4a05dc09376e5007dc3188db4b756
Stewart Young <stewartyoung97@gmail.com>
2021-12-02 17:47:54 +0000

    add answer for part two of 2nd in java

--- 2/src/io/stewartyoung/Main.java
+++ 2/src/io/stewartyoung/Main.java
@@ -10,34 +10,47 @@ public class Main {
 	// write your code here
         int horizontal = 0;
         int depth = 0;
+        int aim = 0;
+        int depth2 = 0;
         File input = new File("data/input.txt");
         Scanner reader = new Scanner(input);
         while (reader.hasNextLine()) {
             String data = reader.nextLine();
             String[] dataArray = data.split("\\s+");
             String direction = dataArray[0];
-            String quantity = dataArray[1];
+            int quantity = Integer.parseInt(dataArray[1]);
             System.out.println(direction);
             System.out.println(quantity);
 
             switch(direction){
                 case "up":
-                    depth = depth - Integer.parseInt(quantity);
+                    depth = depth - quantity;
+//                    depth2 = depth2 - quantity;
+                    aim = aim - quantity;
                     break;
                 case "down":
-                    depth = depth + Integer.parseInt(quantity);
+                    depth = depth + quantity;
+//                    depth2 = depth2 + quantity;
+                    aim = aim + quantity;
                     break;
                 case "forward":
-                    horizontal = horizontal + Integer.parseInt(quantity);
+                    horizontal = horizontal + quantity;
+                    depth2 = depth2 + aim * quantity;
                     break;
             }
 
             System.out.println("horizontal: " + horizontal);
             System.out.println("depth: " + depth);
+            System.out.println("depth2: " + depth2);
+            System.out.println("aim: " + aim);
         }
         System.out.println("horizontal: " + horizontal + " depth: " + depth);
         int horizontalXDepth = horizontal * depth;
         System.out.println(horizontalXDepth);
+
+        int horizontalXDepth2 = horizontal * depth2;
+        System.out.println(horizontalXDepth2);
+
         reader.close();
     }
 }

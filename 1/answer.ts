766112c82d7f044f01a9b2df0a8e24af5836382e
Stewart Young <stewartyoung97@gmail.com>
2021-12-02 10:37:45 +0000

    correct part one answer for 1st Dec

--- 1/answer.ts
+++ 1/answer.ts
@@ -1,4 +1,16 @@
 #!/usr/bin/env ts-node
 import * as fs from 'fs';
 const file = fs.readFileSync("input.txt", "utf8");
-console.log(file)
+const nums = file.split("\n");
+let current = 0;
+let next = 1;
+let largerCount = 0;
+while (next < nums.length) {
+    // use unary operator + to convert to number
+    if (+nums[current] < +nums[next]) {
+        largerCount += 1;
+    }
+    current +=1;
+    next += 1;
+}
+console.log(largerCount);
